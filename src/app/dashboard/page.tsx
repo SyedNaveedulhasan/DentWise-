a5867600f0ee67df46f6479136c549208a97a093
SyedNaveedulhasan <syednaveedulhasan123@gmail.com>
2025-12-28 20:50:28 +0500

    dashboard page added

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,17 @@
+import ActivityOverview from '@/components/dashboard/ActivityOverview'
+import MainActions from '@/components/dashboard/MainActions'
+import WelcomeSection from '@/components/dashboard/WelcomeSection'
 import Navbar from '@/components/Navbar'
 
 const DashboardPage = () => {
   return (
     <>
     <Navbar/>
-    <div>DashboardPage</div>
+    <div className='max-w-7xl mx-auto px-6 py-8 pt-24'>
+      <WelcomeSection/>
+      <MainActions/>
+      <ActivityOverview/>
+    </div>
     </>
   )
 }
